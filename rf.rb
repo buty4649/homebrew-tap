750c1c646a888ea3b316c352271e67fe6efdddd0
buty4649 <buty4649@gmail.com>
2023-09-05 22:04:19 +0900

    update

--- rf.rb
+++ rf.rb
@@ -1,31 +1,39 @@
 class Rf < Formula
   desc 'rf is Ruby powered text/json/yaml filter'
-  version '1.4.0'
+  version '1.5.0'
   homepage 'https://github.com/buty4649/rf'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/buty4649/rf/releases/download/v1.4.0/rf-v1.4.0-darwin-arm64.zip'
-      sha256 'f73e9df5364f25ae033b5fc11c51a3ab65edd77dd6bfee3c400131c9dc1d8985'
+      url 'https://github.com/buty4649/rf/releases/download/v1.5.0/rf-v1.5.0-darwin-arm64.zip'
+      sha256 '4800a3aef3b8a10c30209dda47f8f00cc3503b14e8e8e9d5551e210f35182cae'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/buty4649/rf/releases/download/v1.4.0/rf-v1.4.0-darwin-amd64.zip'
-      sha256 '1e8e2cf2d82641a4723100580be83c826f1a054ba1af9dd0867b4da6496c76fc'
+      url 'https://github.com/buty4649/rf/releases/download/v1.5.0/rf-v1.5.0-darwin-amd64.zip'
+      sha256 'b45b3258be7e4fab76efa73dd6e4a3942a24110d712990c4470cf9abfd944679'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/buty4649/rf/releases/download/v1.4.0/rf-v1.4.0-linux-arm64.tar.gz'
-      sha256 '89c0899005be540e68035ec83cacb767aa91eece80e91f15e9a607cb70fad94f'
+      url 'https://github.com/buty4649/rf/releases/download/v1.5.0/rf-v1.5.0-linux-arm64.tar.gz'
+      sha256 'f400ec1793748fc86460c4485888485c37988beed9f75a87559830e0a7c11bf9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/buty4649/rf/releases/download/v1.4.0/rf-v1.4.0-linux-amd64.tar.gz'
-      sha256 '6f3e35917e487f09c176db07ea338afd5211dd5ce9e3dc6f02f49b5ea631291c'
+      url 'https://github.com/buty4649/rf/releases/download/v1.5.0/rf-v1.5.0-linux-amd64.tar.gz'
+      sha256 '7c65f6690fa6747c0bd9d5bfa3e77ca8b54de41b71d5ff18088411e4f58fce0b'
     end
   end
 
+  head do
+    url 'https://github.com/buty4649/rf.git'
+    depends_on 'go' => :build
+  end
+
   def install
+    if build.head?
+      system 'make', 'build'
+    end
     bin.install 'rf'
   end
 end
